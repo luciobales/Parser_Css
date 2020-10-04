5cc0a95ab4909e5b5e6f7ce0b41ed38b60fffbcc
EtcGonza <xetchegaraygonzalo@gmail.com>
2020-10-04 11:33:20 -0300

    Hice ejemplos

--- prueba.hs
+++ prueba.hs
@@ -1,6 +1,32 @@
 -- Ingreso => "clase_boton-rojo"
 -- y separar la cadena segun el '_'
 
+-- La estructura que tendria que formar seria la siguiente:
+-- .boton-rojo {
+-- 
+-- }
+
+-- Otro ejemplo
+-- La estructura que tendria que formar seria la siguiente:
+-- Ingreso "div" 
+-- div {
+-- 
+-- }
+
+-- Otro ejemplo
+-- La estructura que tendria que formar seria la siguiente:
+-- Ingreso "id_contenedor" 
+-- #contenedor {
+-- 
+-- }
+
+-- Otro ejemplo
+-- La estructura que tendria que formar seria la siguiente:
+-- Ingreso "div_clase_contenedor-rojo" 
+-- div.contenedor-rojo {
+-- 
+-- }
+
 buscarGuinBajo :: [Char] -> [Char]
 buscarGuinBajo [] = []
 buscarGuinBajo (x:xs) = 
